76be22839b3788f7786fa49ae95548233cc5194f
drewmcfall <dmlegomaniac@gmail.com>
2015-06-15 15:36:12 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '7Rvo7i3qaH8QeteaZJgIYAGv6OuZwGuMcOyaERxZ9IzVs6Il', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1513771212], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Data Stream1', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '990000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
